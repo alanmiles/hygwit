cba821d0251e769a1ed1c80303a28a429bbb817c
Alan Miles <alanpqs@gmail.com>
2012-10-29 10:21:27 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hygwit::Application.config.secret_token = '553d20d29d572e6ec8023bea0ab13d0741c586ef47d5429de2bff1ec930e7f15cb1aae2c00fee6feceeaa9990db7b5a51ff68ef8572dada7da8d659446504b95'
