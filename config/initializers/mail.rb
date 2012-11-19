4f8069e67e74290d0c4492ebe824d5976ddc4972
Alan Miles <alanpqs@gmail.com>
2012-11-19 17:42:30 +0000

    Change password in inits

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,8 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '465',
   :authentication => :plain,
-  :user_name      => ENV['SENDGRID_USERNAME'],
-  :password       => ENV['SENDGRID_PASSWORD'],
+  :user_name      => 'app8810110',
+  :password       => '7iwcosdx',
   :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
