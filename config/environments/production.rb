f20975a2d563a74180d69e0050b8a74aeba5042b
Alan Miles <alanpqs@gmail.com>
2012-11-19 16:45:54 +0000

    changed config env production for SMTP

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -51,7 +51,9 @@ Hygwit::Application.configure do
 
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
-
+  
+  config.action_mailer.delivery_method = :smtp 
+  config.action_mailer.raise_delivery_errors = true 
   # Enable threaded mode
   # config.threadsafe!
 
