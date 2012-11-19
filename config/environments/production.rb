e0228a21fa7517bbe38f908d97563d08bff4482a
Alan Miles <alanpqs@gmail.com>
2012-11-19 17:07:36 +0000

    Added to mail.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -52,8 +52,7 @@ Hygwit::Application.configure do
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
   
-  config.action_mailer.delivery_method = :smtp 
-  config.action_mailer.raise_delivery_errors = true 
+  #Config.action_mailer.raise_delivery_errors = true 
   # Enable threaded mode
   # config.threadsafe!
 
