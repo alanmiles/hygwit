0f2074894b827930694680cb26c83ee597679b7a
Alan Miles <alanpqs@gmail.com>
2012-11-19 17:51:40 +0000

    removed link to site from env

--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,7 +2,7 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '465',
   :authentication => :plain,
-  :user_name      => 'app8810110',
+  :user_name      => 'app8810110@heroku.com',
   :password       => '7iwcosdx',
   :domain         => 'heroku.com'
 }
