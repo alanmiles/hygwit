1f6d90c4fa38f7643af6576777d25d9792274be3
Alan Miles <alanpqs@gmail.com>
2012-11-20 01:33:25 +0000

    create initializer file for Sendgrid

--- config/environments/production.rb~
+++ config/environments/production.rb~
@@ -59,8 +59,8 @@ Hygwit::Application.configure do
     :address        => 'smtp.sendgrid.net',
     :port           => '587',
     :authentication => :plain,
-    :user_name      => 'app8810110@heroku.com',
-    :password       => '7iwcosdx',
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
     :domain         => 'heroku.com',
     :enable_starttls_auto => true
   }
--- config/initializers/mail.rb
+++ config/initializers/mail.rb
@@ -2,11 +2,8 @@ ActionMailer::Base.smtp_settings = {
   :address        => 'smtp.sendgrid.net',
   :port           => '587',
   :authentication => :plain,
-  :user_name      => 'app8810110@heroku.com',
-  :password       => '7iwcosdx',
-  :domain         => 'heroku.com',
-  :enable_starttls_auto => true
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com'
 }
 ActionMailer::Base.delivery_method = :smtp
-
-
