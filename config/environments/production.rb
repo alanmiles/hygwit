142bc956854ab93cc388795f56e8b83ef469d8bd
Alan Miles <alanpqs@gmail.com>
2012-11-20 02:03:59 +0000

    revert to production env

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,22 +53,23 @@ Hygwit::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   #config.action_mailer.default_url_options = { :host => "https://pure-headland-1160.herokuapp.com/" }
   
-  #config.action_mailer.delivery_method = :smtp
-  
-  #config.action_mailer.smtp_settings = {
-  #  :address        => 'smtp.sendgrid.net',
-  #  :port           => '587',
-  #  :authentication => :plain,
-  #  :user_name      => ENV['SENDGRID_USERNAME'],
-  #  :password       => ENV['SENDGRID_PASSWORD'],
-  #  :domain         => 'heroku.com',
-  #  :enable_starttls_auto => true
-  #}
-  
+  config.action_mailer.delivery_method = :smtp
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
  
-  config.action_mailer.default_url_options = { :host => "https://pure-headland-1160.herokuapp.com/" }
+  config.action_mailer.default_url_options = { :host => "heroku.com" }
+  
+  config.action_mailer.smtp_settings = {
+    :address        => 'smtp.sendgrid.net',
+    :port           => '465',
+    :authentication => :plain,
+    :user_name      => ENV['SENDGRID_USERNAME'],
+    :password       => ENV['SENDGRID_PASSWORD'],
+    :domain         => 'heroku.com',
+    :enable_starttls_auto => true
+  }
+  
+  
   # Enable threaded mode
   # config.threadsafe!
 
--- config/environments/production.rb~
+++ config/environments/production.rb~
@@ -53,17 +53,17 @@ Hygwit::Application.configure do
   # config.action_mailer.raise_delivery_errors = false
   #config.action_mailer.default_url_options = { :host => "https://pure-headland-1160.herokuapp.com/" }
   
-  config.action_mailer.delivery_method = :smtp
+  #config.action_mailer.delivery_method = :smtp
   
-  config.action_mailer.smtp_settings = {
-    :address        => 'smtp.sendgrid.net',
-    :port           => '587',
-    :authentication => :plain,
-    :user_name      => ENV['SENDGRID_USERNAME'],
-    :password       => ENV['SENDGRID_PASSWORD'],
-    :domain         => 'heroku.com',
-    :enable_starttls_auto => true
-  }
+  #config.action_mailer.smtp_settings = {
+  #  :address        => 'smtp.sendgrid.net',
+  #  :port           => '587',
+  #  :authentication => :plain,
+  #  :user_name      => ENV['SENDGRID_USERNAME'],
+  #  :password       => ENV['SENDGRID_PASSWORD'],
+  #  :domain         => 'heroku.com',
+  #  :enable_starttls_auto => true
+  #}
   
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
